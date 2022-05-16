c58edf8963844f3910f6c1999b76ddf296cf9502
hramadan <hramadan@newrelic.com>
2022-05-16 11:33:25 -0700

    Add install guide

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :apm_logs_in_context, only: [:index, :show]
   resources :generate_traffic, only: [:index]
   resources :async, only: [:index]
+  resources :agent_install, only: [:index]
 end
